67bbe4685553e7472c2987b74a92683dc9be4ad7
Gus Cavanaugh <gustafcavanaugh@gmail.com>
2023-01-12 08:35:39 +0000

    foo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ os.environ["FOOD"]="SPINACH"
 
 def food():
     x = os.environ["FOOD"]
-    print (x)
+    #print (x)
     return x
 
 import os
@@ -14,5 +14,5 @@ value = 'my_value'
 
 if __name__ == "__main__":
     result = food()
-    print(f'::set-output name=FOOD::{result}')
-    print(f'"foo_bar=yellow" >> $GITHUB_ENV')
+    #print(f'::set-output name=FOOD::{result}')
+    print("foo_bar=yellow")
