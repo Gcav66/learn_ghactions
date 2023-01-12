c984da97816a5813c50f580564e46f8b59c1175e
Gus Cavanaugh <gustafcavanaugh@gmail.com>
2023-01-12 08:41:30 +0000

    cleaned up

--- app.py
+++ app.py
@@ -4,15 +4,8 @@ os.environ["FOOD"]="SPINACH"
 
 def food():
     x = os.environ["FOOD"]
-    #print (x)
     return x
-
-import os
-name = 'my_name'
-value = 'my_value'
-
 
 if __name__ == "__main__":
     result = food()
-    #print(f'::set-output name=FOOD::{result}')
     print(f"foo_bar={result}")
