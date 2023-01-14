e7efea8ab858bf711c5c0730fe11b1ca837b4d65
Gus Cavanaugh <gustafcavanaugh@gmail.com>
2023-01-14 17:22:43 +0000

    added second env

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ import os
 
 os.environ["FOOD"]="SPINACH"
 
-def food():
-    x = os.environ["FOOD"]
-    return x
+def meal(my_drink):
+    """Take as input a beverage from the user"""
+    my_env_var = os.environ["FOOD"]
+    user_input = my_drink
+    my_data = {"env_var": my_env_var,
+               "user_input": user_input}
+    return my_data
 
 if __name__ == "__main__":
-    result = food()
-    print(f"foo_bar={result}")
+    result = meal("Sweet Tea")
+    print(f"env_var={result['env_var']}")
+    print(f"user_input={result['user_input']}")
